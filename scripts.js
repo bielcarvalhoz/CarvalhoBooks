d534a10f08fef9fa31713dc4c60017799f4bda8c
Gabriel Carvalho <gcarv137@gmail.com>
2025-10-30 22:24:32 -0300

    formatação do código em scripts.js

--- scripts.js
+++ scripts.js
@@ -1,13 +1,13 @@
-const swiper = new Swiper('.swiper', {
-    spaceBetween: 10,
-    slidesPerView: 3,
-    loop: true,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets'
-    }
+const swiper = new Swiper(".swiper", {
+  spaceBetween: 10,
+  slidesPerView: 3,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+  },
 });
